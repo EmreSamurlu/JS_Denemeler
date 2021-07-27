6eb940d23f9b31326d09528d808e08f427883f13
emre <emrsmrl@gmail.com>
2021-07-28 00:41:19 +0300

    fonksiyon bölüm sonu

---
+++ js files/23-Functions-bolum-sonu.js
@@ -0,0 +1,30 @@
+// Fonsiyon Bölüm Sonu Egzersizi
+let counter = 0 ;
+let counterDOM = document.querySelector('#counter') ;
+let increaseDOM = document.querySelector('#increase');
+let decreaseDOM = document.querySelector('#decrease');
+
+counterDOM.innerHTML = counter
+
+increaseDOM.addEventListener("click", clickEvent)
+decreaseDOM.addEventListener("click", clickEvent)
+
+// * Veya Fonksiyon Şu Şekilde Yazılabilir:
+
+function clickEvent() {
+    console.log(this.id)
+    this.id == "increase" ?  counter += 1 : counter -= 1 ;
+    counterDOM.innerHTML = counter 
+}
+
+
+/* 
+function clickEvent() {
+    console.log(this.id)
+    if (this.id == "increase") {
+        counterDOM.innerHTML = counter += 1 
+    }else {
+        counterDOM.innerHTML = counter -= 1
+    }
+};
+ */
