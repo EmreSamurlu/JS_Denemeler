a57416a0190d5879efec9ea43f7e72ff2908f5c3
emre <emrsmrl@gmail.com>
2021-07-27 23:34:54 +0300

    27.07.2021 js 22 DOM events update

--- js files/22-DOM-events-kullanimi.js
+++ js files/22-DOM-events-kullanimi.js
@@ -2,9 +2,9 @@
 //* https://www.w3schools.com/jsref/dom_obj_event.asp
 //* https://developer.mozilla.org/en-US/docs/Web/Events
 
-
+/* 
 let greeting = document.querySelector("#greeting")
-
+ */
 //* mouse etkinliği
 
 // 1. metot
@@ -26,12 +26,31 @@ function domClick() {
 }
  */
 
-
+/* 
 //* klavye etkinliği /// nasıl çalıştığını gör.. eventler bitmedi aynı çalışmaya devam et. (20.07.2021)
 
 const keyboard = document.querySelector("#info");
+
+//* Events
+
+keyboard.addEventListener("copy", cuttingFunction);
 keyboard.addEventListener("cut", cuttingFunction);
+keyboard.addEventListener("paste", cuttingFunction);
+
+//* Daha farklı eventler mevcut
 
 function cuttingFunction(event) {
     console.log("etkinlik tipi: ", event.type);
+}
+
+ */
+
+//* Form etkinlikleri
+
+const color = document.querySelector("#select");
+color.addEventListener("change", selectBox);
+
+function selectBox(event){
+    console.log("Etkinlik Tipi : " + event.type);
+    console.log("değer : " + event.target.value);
 }
