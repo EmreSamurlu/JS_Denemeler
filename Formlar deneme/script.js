5a7ca2a103261256975c1dac268f994c00b463b5
emre <emrsmrl@gmail.com>
2021-08-02 10:24:43 +0300

    not solved continue later.

--- Formlar deneme/script.js
+++ Formlar deneme/script.js
@@ -1,28 +1,30 @@
+
 let userFormDOM = document.querySelector("#userForm")
 userFormDOM.addEventListener('submit', formHandler)
 const alertDOM = document.querySelector('#alert')
 
 const alertFunction = (title, message, className="warning") => `
 <div class="alert alert-${className} alert-dismissible fade show" role="alert">
-<strong>${title}</strong> ${message}
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <strong>${title}</strong> ${message}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 `
 
 function formHandler(event) {
     event.preventDefault()
-    const USER_NAME = document.querySelector('#userName')
-    const SCORE = document.querySelector('#score')
-    
-    if (USER_NAME.value && SCORE.value){
-            addItem(USER_NAME.value, SCORE.value) // gönderdikten sonra sıfırladık
-            USER_NAME.value = ""
-            SCORE.value = ""
-            alertDOM.innerHTML = alertFunction("Giriş Bilgisi","Bilgiler Başarı ile Girildi!", "success")
-        }else{
-            alertDOM.innerHTML = alertFunction("Giriş Bilgisi","Eksik Bilgi Girişi Yapıldı!", "danger")
-        }
+    const USER_NAME = document.querySelector("#username")
+    const SCORE = document.querySelector("#score")
+
+    if (USER_NAME.value && SCORE.value >= 0 && SCORE.value <=100){
+        addItem(USER_NAME.value, SCORE.value) // gönderdikten sonra sıfırladık
+        USER_NAME.value = ""
+        SCORE.value = ""
+        alertDOM.innerHTML = alertFunction("Giriş Bilgisi","Bilgiler Başarı ile Girildi!", "success")
+    }else{
+        alertDOM.innerHTML = alertFunction("Giriş Bilgisi","Eksik veya Hatalı Bilgi Girişi Yapıldı!", "danger")
     }
+}
+
 
 let userListDOM = document.querySelector('#userList')
 const addItem = (userName, score) => {
