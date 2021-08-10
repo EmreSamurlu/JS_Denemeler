62042658d23fddcead1a4ee5567bbd4d2176d391
emre <emrsmrl@gmail.com>
2021-08-10 07:31:38 +0300

    object nedir?

---
+++ js files/9-Nesne/39-object-nedir-nasil-olusturulur.js
@@ -0,0 +1,20 @@
+//* object nedir? nasıl oluşturulur?
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+
+
+let arrayObj = [1,2,3];
+let object1 = {obj: 1};
+
+console.log("arrayObj", typeof(arrayObj))
+console.log("object1", typeof(object1))
+console.log("object1", object1);
+
+let item1 = new Object();
+let item2 = {};
+
+console.log("item1", typeof(item1));
+console.log("item2", typeof(item2));
+
+let item3 = {}
+let item4 = new Object()
+
